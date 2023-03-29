86747d2147c797fb1ebb32dcd6191563c3a22bd7
Terry <livemylive@msn.com>
2023-03-29 09:06:56 +0800

    添加项目文件。

---
+++ Models/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColdShineSoft.Models
+{
+	public enum Role
+	{
+		Server,
+		System,
+		User,
+		Assistant,
+		Error
+	}
+}
---
+++ Services/BaseService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColdShineSoft.Services
+{
+	public abstract class BaseService
+	{
+		protected readonly OpenAI.GPT3.Managers.OpenAIService OpenAIService;
+
+		public BaseService(OpenAI.GPT3.Managers.OpenAIService openAIService)
+		{
+			this.OpenAIService = openAIService;
+		}
+	}
+}
